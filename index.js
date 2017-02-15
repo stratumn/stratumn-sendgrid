c95cbcfe6ac36c7ab73bac5ada59c3e25788ee2c
Stephan Florquin <stephan.florquin@gmail.com>
2017-02-15 18:03:59 +0100

    change license to Mozilla Public License 2.0

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/*
+    Copyright (C) 2017  Stratumn SAS
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 const sendgrid = require('sendgrid')(process.env.SENDGRID_API_KEY);
 
 module.exports = {
